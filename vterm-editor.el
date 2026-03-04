dc10185111e4f0014528d28a3e31d26c48d5e2ef
Andros Fenollosa <hi@andros.dev>
2026-03-04 15:46:03 +0100

    Bump version to 1.0.0, add GPL-3.0 license and boilerplate

--- vterm-editor.el
+++ vterm-editor.el
@@ -1,12 +1,25 @@
 ;;; vterm-editor.el --- Edit text in a buffer and send it to vterm -*- lexical-binding: t; -*-
 
 ;; Author:
-;; URL: https://github.com/tanrax/vterm-editor.el
-;; Version: 0.1.0
+;; URL: https://git.andros.dev/andros/vterm-editor.el
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "25.1") (vterm "0.0.1"))
 ;; Keywords: terminals, convenience
 
 ;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
